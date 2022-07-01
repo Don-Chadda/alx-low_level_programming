0c3631622e80a7ca7303b48777bd87e7977041ac
Don-Chadda <peterturay81@gmail.com>
2022-07-01 10:47:08 -0700

    C variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main prints the alhabet in lowercase, followed by a
+ * new line
+ * Return is always 0
+ */
+int main(void)
+{
+	char cAL;
+
+	for (cAL = 'a'; cAL++)
+	{
+		putchar(cAL);
+	}
+	putchar('\n');
+	return (0);
+}
