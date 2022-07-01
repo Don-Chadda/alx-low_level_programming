243b4c7d5b26a2aae3d82708b333797fb29fc8f6
Don-Chadda <peterturay81@gmail.com>
2022-07-01 10:00:04 -0700

    C variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main - assigns a random number to int n everytime
- * it excutes, and prints it
+/**
+ * main assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
