220df9ee6bfaf8960a2c97a0b1e89c019b3c1d95
Peter Daniel Chadda Turay <69143593+Don-Chadda@users.noreply.github.com>
2022-07-08 17:12:17 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _iasuper - checks for upercase character
+ * _isupper - checks for uppercase character
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
