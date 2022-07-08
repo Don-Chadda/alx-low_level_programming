b2cf58b98e86b4d2146fb8dc0da194355cb8079f
Don-Chadda <peterturay81@gmail.com>
2022-07-08 10:02:32 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _iasuper - checks for upercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _super(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
